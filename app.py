6e567c56213e0d8cc44a9f229fbdf38d19b5013b
atharv-naik <atharvn317@gmail.com>
2023-04-08 11:15:24 +0530

    minor improvements

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def upload_file():
         return render_template('index.html', error='')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
